e181b3d7a791f99f037ac05b780ccc66c43120c6
tanxeem <you@example.com>
2024-12-24 23:58:00 +0300

    eslint setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,15 @@
-import express from 'express'
-import { StatusCodes } from 'http-status-codes'
+import express from 'express';
 import { PORT } from './config/serverConfig.js';
+import { StatusCodes } from 'http-status-codes';
+
+
 
 const app = express();
 
+app.get('/ping', (req, res) => {
+  return res.status(StatusCodes.OK).json({ message: 'pong' });
+});
 
-
-app.get("/ping",(req, res) => {
-    return res.status(StatusCodes.OK).json({ message: 'pong' })
-})
-
-
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
