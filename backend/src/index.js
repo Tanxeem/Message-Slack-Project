b9653fa44e6c06b5648f677b64d8f9fa39c8aa4d
tanxeem <you@example.com>
2024-12-25 19:09:05 +0300

    Edit basic routing layer

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
-import { PORT } from './config/serverConfig.js';
 import { StatusCodes } from 'http-status-codes';
 
-
+import connectDB from './config/dbConfig.js';
+import { PORT } from './config/serverConfig.js';
+import apiRouter from './routes/apiRouter.js'
 
 const app = express();
 
+// middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/api', apiRouter)
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
